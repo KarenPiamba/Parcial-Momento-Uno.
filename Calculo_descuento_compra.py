054253b92f6bf00b50291dffed2ad25af3402fe5
KarenPiamba <karen.piambaa@uniagustiniana.edu.co>
2025-03-06 20:53:23 -0500

    Update Calculo_descuento_compra.py

--- Calculo_descuento_compra.py
+++ Calculo_descuento_compra.py
@@ -1,14 +1,14 @@
 #Entrada
-compra=int(input("Ingresa el monto total de la compra"))
+compra=int(input("Ingresa el monto total de la compra: "))
 if(compra<50):
     print("No hay descuento")
 elif(compra>=50 and compra<=100):
     descuento=compra*0.05
     total=compra-descuento
-    print("El descuento del cinco porciento es:",descuento)
-    print("El total a pagar en la compra es:",total)
+    print("Descuento aplicado:",descuento)
+    print("Total a pagar:",total)
 elif(compra>100):
     descuento=compra*0.1
     total=compra-descuento
-    print("El descuento del cinco porciento es:",descuento)
-    print("El total a pagar en la compra es:",total)
+    print("Descuento aplicado:",descuento)
+    print("Total a pagar:",total)
